33d840c40eb838787bfb13e98db069581be80dac
EthanolCB <ethanlalla300@gmail.com>
2021-12-05 19:43:55 -0400

    added html lists added js for word arrays

---
+++ script.js
@@ -0,0 +1,3 @@
+
+var secretArray = document.getElementsByClass('wrd');
+var secretArray = [tattoo, camera, picture, blanket, pillow, brown, phone, wallpaper, canvas, bed, marker, battery, tape, plush, keyboard, paper, knob, santa, orange, drawing, polaroid, comfort, shoe, hanger];
