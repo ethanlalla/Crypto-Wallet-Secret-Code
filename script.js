75700aaf31093f1f79eb980d238ac6ed4903cb51
EthanolCB <ethanlalla300@gmail.com>
2021-12-06 00:00:49 -0400

    updated array added for loop

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var secretArray = document.getElementsByClassName("wrd");
+var secretArray = [ 'tattoo, camera, picture, blanket, pillow, brown, phone, wallpaper, canvas, bed, marker, battery, tape, plush, keyboard, paper, knob, santa, orange, drawing, polaroid, comfort, shoe, hanger' ];
+ 
+var secretArray = document.querySelectorAll('li.wrd');
 
-var secretArray = document.getElementsByClass('wrd');
-var secretArray = [tattoo, camera, picture, blanket, pillow, brown, phone, wallpaper, canvas, bed, marker, battery, tape, plush, keyboard, paper, knob, santa, orange, drawing, polaroid, comfort, shoe, hanger];
+
+for (var i = 0; i < secretArray.length; i++){
+secretArray[i].className = "wrd";
+}
