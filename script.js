892066808a763dcdab4ef6b67e5fded720a619c5
EthanolCB <ethanlalla300@gmail.com>
2021-12-06 13:24:35 -0400

    fixed issue of words not populating, they are now randomized but have some repetition

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 var secretArray = document.getElementsByClassName("wrd");
-var secretArray = [ 'tattoo, camera, picture, blanket, pillow, brown, phone, wallpaper, canvas, bed, marker, battery, tape, plush, keyboard, paper, knob, santa, orange, drawing, polaroid, comfort, shoe, hanger' ];
- 
-var secretArray = document.querySelectorAll('li.wrd');
+var secret = ['tattoo', 'camera', 'picture', 'blanket', 'pillow', 'brown', 'phone', 'wallpaper', 'canvas', 'bed', 'marker', 'battery', 'tape', 'plush', 'keyboard', 'paper', 'knob', 'santa', 'orange', 'drawing', 'polaroid', 'comfort', 'shoe', 'hanger'];
 
 
-for (var i = 0; i < secretArray.length; i++){
-secretArray[i].className = "wrd";
+for (var i = 0; i < secretArray.length; i++) {
+  var random = Math.floor(Math.random()*24);
+  secretArray[i].textContent = secret[random];
 }
